9535f670e64a35b5a1096980d4f7901157745d2c
abdi majiid haji <najacade23456@gmail.com>
2025-09-24 13:20:31 +0300

    chnaged

--- src/component/Admin/waterProject/waterProjectForm.jsx
+++ src/component/Admin/waterProject/waterProjectForm.jsx
@@ -116,7 +116,7 @@ export default function WaterProjectFormUI() {
                       id="componentTitle"
                       name="componentTitle"
                       type="text"
-                      required
+                    
                      
                       className="mt-2 w-full rounded-lg border px-3 py-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-[var(--brand)]"
                     />
