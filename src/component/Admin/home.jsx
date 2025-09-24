e2d268d3476dedc6e652386c37a81943778dee9c
abdi majiid haji <najacade23456@gmail.com>
2025-09-24 11:12:20 +0300

    chnaged

--- src/component/Admin/home.jsx
+++ src/component/Admin/home.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 export default function DashboardCards() {
-  const API = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+  const API =  "https://moewr-backend.onrender.com";
 
   // --- axios client with token ---
   const http = useMemo(() => {
