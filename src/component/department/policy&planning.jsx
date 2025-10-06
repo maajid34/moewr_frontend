8d66380831785291c0e9641b9b29a64e36052052
abdi majiid haji <najacade23456@gmail.com>
2025-10-06 14:51:20 +0300

    chnaged

--- src/component/department/policy&planning.jsx
+++ src/component/department/policy&planning.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import img1 from '../../assets/man-writing-notebook.jpg'
 import img2 from '../../assets/planbg.png'
 import MoewrFooter from '../../pages/footer';
